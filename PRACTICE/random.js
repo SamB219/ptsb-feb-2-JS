452c64eeba43a369c8da415c922a10324af8953f
SamB219 <morebagelsfr@gmail.com>
2024-03-25 20:57:25 -0400

    new notes just droppped

---
+++ PRACTICE/random.js
@@ -0,0 +1,31 @@
+
+
+//fizzbuzz challenge 
+
+// function fizzBuzz() {
+//     for (let i = 1; i <= 100; i++) {
+//         if (i % 3 === 0 && i % 5 === 0) {
+//             console.log("Fizz Buzz :P");
+//         } else if(i % 3 === 0) {
+//             console.log("Fizz");
+//         } else if(i % 5 === 0) {
+//             console.log("Buzz");
+//         } else {
+//             console.log(i)
+//         }
+        
+//     }
+// }
+
+// fizzBuzz()
+
+
+// restaurant tip calc
+
+const bill = 18.00;
+
+function tipAmount(bill, tipPercentage) {
+    return console.log(bill * tipPercentage.toFixed(1));
+}
+
+tipAmount(bill, .20)
